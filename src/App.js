51ccc49abaf95b6b5d3c349050732f0f725915a4
Tamer545 <8235.Lohn>
2021-11-02 14:35:30 +0100

    adding the basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {Button} from "react-bootstrap"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState(0)
+    const [moneyPerSecond, setMoneyPerSecond] = useState(0)
+
+    useEffect(() => {
+        const id = setInterval(() => setMoney((oldMoney) => oldMoney + moneyPerSecond), 1000)
+
+        return () => clearInterval(id);
+    }, [moneyPerSecond])
+
+    function Upgrade(props){
+        function levelUp(){
+            if (money >= props.price) {
+                setMoney(money - props.price)
+                setMoneyPerSecond(moneyPerSecond + props.generatingPerSec)
+                console.log("Dieses Programm ist Copyright geschützt durch Kai Bria")
+            }
+        }
+
+
+        return(
+            <Button onClick={() => levelUp(props.generatingPerSec, props.price)}>{props.upgradeName}</Button>
+        )
+
+    }
+
+    return (
+        <div className="App">
+            {money}
+            <Button onClick={() => setMoney(money + 1)}>Generieren</Button>
+            <Upgrade generatingPerSec={1} price={10} upgradeName={"Upgrade 1"}></Upgrade>
+            <Upgrade generatingPerSec={5} price={50} upgradeName={"Upgrade 2"}/>
+            <Upgrade generatingPerSec={10} price={200} upgradeName={"Upgrade 3"}></Upgrade>
+            <Upgrade generatingPerSec={60} price={4000} upgradeName={"Upgrade 4"}/>
+            <Upgrade generatingPerSec={100} price={46000} upgradeName={"Upgrade 5"}></Upgrade>
+            <Upgrade generatingPerSec={230} price={120000} upgradeName={"Upgrade 6"}></Upgrade>
+        </div>
+    );
 }
 
 export default App;
